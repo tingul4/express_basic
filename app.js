1ed037fce66bc83df7555210d67f42b473232e96
tingul4 <xpple413208@gmail.com>
2023-06-07 18:55:03 +0800

    set routing demo

--- app.js
+++ app.js
@@ -7,7 +7,18 @@ const port = 3000
 app.get('/', (req, res) => {
   res.send(`This is my first Express Web App`)
 })
+/* add some route */
+app.get('/', (req, res) => {
+  res.send('This is my first Express Web App')
+})
 
+app.get('/food', (req, res) => {
+  res.send('My favorite food is ice cream')
+})
+
+app.get('/popular/languages', (req, res) => {
+  res.send('JavaScript is a popular language')
+})
 // Listen the server when it started
 app.listen(port, () => {
   console.log(`Express is running on http://localhost:${port}`)
